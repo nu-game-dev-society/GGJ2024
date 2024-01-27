43efdcd3c9cdb50f1f7cf19e36ff9ada6008300c
Patton97 <atpatton97@gmail.com>
2024-01-27 20:00:04 +0000

    Initial interactable stuff on hookaduck

---
+++ Assets/Scripts/HookADuck/HookADuckPoleInteractionComponent.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class HookADuckPoleInteractionComponent : MonoBehaviour, IInteractable
+{
+    public void Interact(PlayerController interactor)
+    {
+        
+    }
+
+    public string PopupText()
+    {
+        return "Play 'Hook A Duck'";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
