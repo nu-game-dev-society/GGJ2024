82cfc1fe5cc1d25c1db78f7dd465f2d5394d27c3
rtm516 <ryantmilner@hotmail.co.uk>
2024-01-27 14:50:50 +0000

    Add basketball prefabs

---
+++ Assets/Scripts/Basketball/BasketballUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BasketballUI : MonoBehaviour
+{
+    public Transform ball;
+    public Transform barStart;
+    public Transform barEnd;
+}
